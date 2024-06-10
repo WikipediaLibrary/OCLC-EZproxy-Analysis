c0ff383c8a33b9e49f430271ca68cff7252c8007
jsn <jsherman@wikimedia.org>
2024-06-10 12:35:02 -0500

    unique_users: load creds from .env, change output directory, supress tls warnings.

--- unique_users.py
+++ unique_users.py
@@ -1,6 +1,12 @@
 import datetime
 import requests
+from dotenv import load_dotenv, dotenv_values
+import os
+import requests
+import urllib3
 
+load_dotenv()
+urllib3.disable_warnings()
 
 def date_range_list(start_date, end_date):
     # Return list of datetime.date objects between start_date and end_date (inclusive).
@@ -12,8 +18,8 @@ def date_range_list(start_date, end_date):
     return date_list
 
 
-start_date = datetime.date(year=2022, month=11, day=1)
-stop_date = datetime.date(year=2023, month=1, day=31)
+start_date = datetime.date(year=2023, month=12, day=1)
+stop_date = datetime.date(year=2024, month=6, day=1)
 date_list = date_range_list(start_date, stop_date)
 
 s = requests.Session()
@@ -24,8 +30,8 @@ log_list_url = 'https://wikipedialibrary.idm.oclc.org/loggedin/admlog/loglisting
 # Get cookie
 s.get(login_url, verify=False)
 
-# Login - add details
-login_response = s.post(login_url, data={'user': '', 'pass': ''}, verify=False)
+# Login - set credentials in .env
+login_response = s.post(login_url, data={'user': os.getenv('user', ''), 'pass': os.getenv('password', '')}, verify=False)
 
 response = s.get(log_list_url, verify=False)
 
@@ -33,4 +39,4 @@ session_partner_url = "https://wikipedialibrary.idm.oclc.org/loggedin/admaudit/{
 user_session_url = "https://wikipedialibrary.idm.oclc.org/loggedin/admlog/spu{}.log"  # Can also be .gz  # Needs to be manually downloaded.
 for date in date_list:
     session_partner_file = s.get(session_partner_url.format(date))
-    open('q4/{}.txt'.format(date), 'wb').write(session_partner_file.content)
+    open('data/{}.txt'.format(date), 'wb').write(session_partner_file.content)
