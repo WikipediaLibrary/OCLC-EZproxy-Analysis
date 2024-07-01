ef60fc4b733fd93ca937afad5daf07a1083433c3
jsn <jsherman@wikimedia.org>
2024-07-01 13:48:09 -0500

    parse_users: config and error handling
    - set date_range in .env
    - set partner_search in .env
    - continue on missing files

--- parse_users.py
+++ parse_users.py
@@ -1,18 +1,19 @@
-import datetime
-
+from datetime import date, timedelta
+import os
+from pathlib import Path
 
 def date_range_list(start_date, end_date):
-    # Return list of datetime.date objects between start_date and end_date (inclusive).
+    # Return list of date objects between start_date and end_date (inclusive).
     date_list = []
     curr_date = start_date
     while curr_date <= end_date:
         date_list.append(curr_date.strftime('%Y%m%d'))
-        curr_date += datetime.timedelta(days=1)
+        curr_date += timedelta(days=1)
     return date_list
 
 
-start_date = datetime.date(year=2022, month=11, day=1)
-stop_date = datetime.date(year=2023, month=1, day=31)
+start_date = date.fromisoformat(os.getenv("start_date", "20240101"))
+stop_date = date.fromisoformat(os.getenv("end_date", "20240630"))
 date_list = date_range_list(start_date, stop_date)
 
 file_path = 'data'
@@ -34,14 +35,17 @@ for date in date_list:
                 user_sessions[session_id] = username
 
 user_tracker = []
+partner_search = os.getenv("partner_search", "microform.digital")
 
 for date in date_list:
-    user_file = file_path + '/spu{}.log'.format(date)
-
+    user_file = Path(file_path + '/spu{}.log'.format(date))
+    if not user_file.is_file():
+        print("{} not found".format(user_file))
+        continue
     with open(user_file, 'r', encoding='utf-8') as user_file_open:
         file_lines = user_file_open.readlines()
         for line in file_lines:
-            if "wiley.com" in line:
+            if partner_search in line:
                 session_id = " ".join(line.split()).split(" ")[2]
                 username = user_sessions[session_id]
 
