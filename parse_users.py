0305df4daf6e165f5063e1cce4312142a4656637
jsn <jsherman@wikimedia.org>
2024-07-01 13:54:49 -0500

    python black

--- parse_users.py
+++ parse_users.py
@@ -2,12 +2,13 @@ from datetime import date, timedelta
 import os
 from pathlib import Path
 
+
 def date_range_list(start_date, end_date):
     # Return list of date objects between start_date and end_date (inclusive).
     date_list = []
     curr_date = start_date
     while curr_date <= end_date:
-        date_list.append(curr_date.strftime('%Y%m%d'))
+        date_list.append(curr_date.strftime("%Y%m%d"))
         curr_date += timedelta(days=1)
     return date_list
 
@@ -16,14 +17,14 @@ start_date = date.fromisoformat(os.getenv("start_date", "20240101"))
 stop_date = date.fromisoformat(os.getenv("end_date", "20240630"))
 date_list = date_range_list(start_date, stop_date)
 
-file_path = 'data'
+file_path = "data"
 
 user_sessions = {}
 
 for date in date_list:
-    user_file = file_path + '/{}.txt'.format(date)
+    user_file = file_path + "/{}.txt".format(date)
 
-    with open(user_file, 'r', encoding='utf-8') as user_file_open:
+    with open(user_file, "r", encoding="utf-8") as user_file_open:
         file_lines = user_file_open.readlines()
         for line in file_lines:
             if "Login.Success" in line:
@@ -38,11 +39,11 @@ user_tracker = []
 partner_search = os.getenv("partner_search", "microform.digital")
 
 for date in date_list:
-    user_file = Path(file_path + '/spu{}.log'.format(date))
+    user_file = Path(file_path + "/spu{}.log".format(date))
     if not user_file.is_file():
         print("{} not found".format(user_file))
         continue
-    with open(user_file, 'r', encoding='utf-8') as user_file_open:
+    with open(user_file, "r", encoding="utf-8") as user_file_open:
         file_lines = user_file_open.readlines()
         for line in file_lines:
             if partner_search in line:
--- unique_users.py
+++ unique_users.py
@@ -13,66 +13,63 @@ from parser import LogListParser
 load_dotenv()
 urllib3.disable_warnings()
 
-start_date = int(os.getenv('start_date', 20240101 ))
-end_date = int(os.getenv('end_date', 20240630))
+start_date = int(os.getenv("start_date", 20240101))
+end_date = int(os.getenv("end_date", 20240630))
 session = requests.Session()
-hostname = os.getenv('hostname', '')
-login_url = 'https://login.{}/login'.format(hostname)
-log_list_url = 'https://{}/loggedin/admlog/loglisting.htm'.format(hostname)
+hostname = os.getenv("hostname", "")
+login_url = "https://login.{}/login".format(hostname)
+log_list_url = "https://{}/loggedin/admlog/loglisting.htm".format(hostname)
 
 # Get cookie
 session.get(login_url, verify=False)
 # Set form data, including our desired destination
 data = {
-    'user': os.getenv('user', ''),
-    'pass': os.getenv('password', ''),
-    'url': log_list_url,
+    "user": os.getenv("user", ""),
+    "pass": os.getenv("password", ""),
+    "url": log_list_url,
 }
 # Login - set credentials in .env
-log_list_response = session.post(
-    login_url,
-    data=data,
-    verify=False,
-    stream=True
-)
+log_list_response = session.post(login_url, data=data, verify=False, stream=True)
 # Bail on error
 if log_list_response.status_code != 200:
     exit
 # Scrape the log listing
 for line in log_list_response.iter_lines():
     parser = LogListParser()
-    fragment = line.decode('utf-8')
-    log_types = ['spu', 'auditfile']
+    fragment = line.decode("utf-8")
+    log_types = ["spu", "auditfile"]
     # We're interested in specific log files
     if not any(log_type in fragment for log_type in log_types):
         continue
     # capture the href value in the html fragment
     parser.feed(fragment)
     # check the date embedded in the filename
-    match = re.search(r'\d{8}', parser.link)
+    match = re.search(r"\d{8}", parser.link)
     date = int(match.group())
     if date < start_date or date > end_date:
-        print('{} not in date range'.format(date))
+        print("{} not in date range".format(date))
         continue
     # Handle the download
-    log_url = 'https://wikipedialibrary.idm.oclc.org/loggedin/admlog/{}'.format(parser.link)
+    log_url = "https://wikipedialibrary.idm.oclc.org/loggedin/admlog/{}".format(
+        parser.link
+    )
     log_purepath = PurePath(parser.link)
-    log_filepath = 'data/{}'.format(log_purepath.name)
-    if log_purepath.suffix == '.gz':
-        log_filepath = 'data/{}'.format(log_purepath.stem)
+    log_filepath = "data/{}".format(log_purepath.name)
+    if log_purepath.suffix == ".gz":
+        log_filepath = "data/{}".format(log_purepath.stem)
     local_log_file = Path(log_filepath)
     if local_log_file.is_file():
-        print('{} exists; skipping download'.format(log_filepath))
+        print("{} exists; skipping download".format(log_filepath))
         continue
-    print('fetching {}'.format(log_url))
+    print("fetching {}".format(log_url))
     log_file_response = session.get(log_url)
     if log_file_response.status_code != 200:
-        print('{} error; download failed'.format(log_file_response.status_code))
+        print("{} error; download failed".format(log_file_response.status_code))
         continue
-    if log_purepath.suffix == '.gz':
-        print('decompressing {}'.format(log_url))
+    if log_purepath.suffix == ".gz":
+        print("decompressing {}".format(log_url))
         content = gzip.decompress(log_file_response.content)
     else:
         content = log_file_response.content
-    print('saving {}'.format(log_filepath))
-    open(log_filepath, 'wb').write(content)
+    print("saving {}".format(log_filepath))
+    open(log_filepath, "wb").write(content)
